dcd8436de108022098eeb009c512c10d2da1eb22
Matthew Kelly <mkonabike@gmail.com>
2019-04-09 10:38:13 -0500

    added button for event trigger, receiving input values

---
+++ main.js
@@ -0,0 +1,8 @@
+const butt = document.getElementById('butt')
+
+butt.onclick = function () {
+	let name = document.getElementById('name').value
+	let age = document.getElementById('age').value
+	console.log(name, age)
+
+}
