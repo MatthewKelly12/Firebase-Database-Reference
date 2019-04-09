4c2ea93d093b982f4363838cc7a386f7f16a1887
Matthew Kelly <mkonabike@gmail.com>
2019-04-09 11:13:19 -0500

    added button do get users

--- main.js
+++ main.js
@@ -24,7 +24,17 @@ const butt = document.getElementById('butt')
 
 // ADD CLLIK EVENT TO BUTTON, WRITE DATA TO FIREBASE
 butt.onclick = function () {
+	// ON BUTT CLICK, GET VALUES FROM INPUT
 	let name = document.getElementById('name').value
 	let age = document.getElementById('age').value
+	// WRITE VALUES TO FIREBASE
 	writeUserData(name, age)
+}
+
+// CREATE VARIABLE FOR BUTTON
+const buttUser = document.getElementById('buttUser')
+
+// ADD CLLIK EVENT TO BUTTON, WRITE DATA TO FIREBASE
+buttUser.onclick = function () {
+	console.log('click')
 }
